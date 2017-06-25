502668055b66c87e101c535d4fe96c1572f8e3cb
Psychedelicagogoparty <yosi@Yoshimitsu-no-MacBook-Air.local>
2017-06-26 00:58:02 +0900

    setup devise

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,6 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
   # GET /questions
-  # GET /questions.json
   def index
     @questions = Question.all
   end
