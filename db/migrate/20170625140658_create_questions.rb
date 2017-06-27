64f84f048384372b516b3c8f5612e449ae1fbe2f
Psychedelicagogoparty <yosi@Yoshimitsu-no-MacBook-Air.local>
2017-06-27 11:40:04 +0900

    setup devise

--- db/migrate/20170625140658_create_questions.rb
+++ db/migrate/20170625140658_create_questions.rb
@@ -1,8 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string,content :title
-
+      t.string :title
+      t.text :content
       t.timestamps null: false
     end
   end
