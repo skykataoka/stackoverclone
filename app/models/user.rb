87fe95f45da8f2b301c7b77cae21176d597b29ed
KataokaSusumu <skykataoka@gmail.com>
2017-06-28 06:58:38 +0900

    set association to user question favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :question
-  has_many :
+  has_many :favorites
+  has_many :questions
 end
