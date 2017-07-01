50ec97f92dccc1f2e31b5627d077cddf73f34a5a
KataokaSusumu <skykataoka@gmail.com>
2017-07-01 09:38:12 +0900

    fix favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
   def create
     #お気に入りに入れる
-    @favorite = Favorite.create(user_id: current_user.id, tweet_id: params[:tweet_id])
-    @favorites = Favorite.where(tweet_id: params[:tweet_id])
+    @favorite = Favorite.create(user_id: current_user.id, question_id: params[:question_id])
+    @favorites = Favorite.where(question_id: params[:question_id])
     @questions = Question.all
   end
 
   def destroy
     #お気に入りから外す
-    favorite = favorite.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    favorite = favorite.find_by(user_id: current_user.id, question_id: params[:question_id])
     favorite.destroy
-    @favorites = Favorite.where(tweet_id: params[:tweet_id])
+    @favorites = Favorite.where(question_id: params[:question_id])
     @Questions = Question.all
   end
 end
