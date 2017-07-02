b29e6433c2a15cd4f8529140e1849bed3ff3978a
Morita Jun <morita.jn0124@gmail.com>
2017-07-02 12:02:25 +0900

    add destory vote method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -12,5 +12,14 @@ class VotesController < ApplicationController
   end
 
   def destroy
+    destination = params["vote"]["target"]
+    if destination == "question"
+      target = Question.find(params["vote"]["question_id"])
+      redirect_path = target
+    else 
+      # 回答用の処理を記述
+    end
+    Vote.subtract_vote(params["vote"]["id"])
+    redirect_to redirect_path
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,7 +6,7 @@ class Vote < ActiveRecord::Base
     where("votes.question_id = ? AND votes.user_id = ?", question_id, user_id)
   end
 
-  # 投票(加算用)メソッド
+  # 投票メソッド
   def self.add_vote(target_id, user_id, target)
     if target == "question"
       vote = Vote.new(question_id: target_id)
@@ -16,5 +16,11 @@ class Vote < ActiveRecord::Base
     vote.user_id = user_id
     vote.cnt += 1
     vote.save
+  end
+  # 投票取り消しメソッド
+  # todo:減算に対応する。
+  def self.subtract_vote(target_id)
+    vote = Vote.find(target_id)
+    vote.destroy
   end
 end
