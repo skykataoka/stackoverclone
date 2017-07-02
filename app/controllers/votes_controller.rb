70bbfc941c32d45e5d45e02350ee761bd14df2da
Morita Jun <morita.jn0124@gmail.com>
2017-07-02 09:26:51 +0900

    add vote create method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,11 @@
 class VotesController < ApplicationController
   def create
+    if params[target] == "question"
+      target = Question.find(params[question_id])
+    else 
+      target = Answer.find(params[answer_id])
+    end
+    Vote.add_vote(target.id, current_user.id, params[target])
   end
 
   def destroy
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,15 @@ class Vote < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
   belongs_to :user
+
+  def self.add_vote(target_id, user_id, target)
+    if target == "question"
+      vote = Vote.new(question_id: target_id)
+    else
+      vote = Vote.new(answer_id: target_id)
+    end
+    vote.user_id = user_id
+    vote.cnt += 1
+    vote.save
+  end
 end
