ae1341959176fdc960326a9c91b0859c87acbe3b
Psychedelicagogoparty <yosi@Yoshimitsu-no-MacBook-Air.local>
2017-07-05 17:42:18 +0900

    add comment_

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170703092508) do
+ActiveRecord::Schema.define(version: 20170705084129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20170703092508) do
     t.string   "unconfirmed_email"
     t.string   "name"
     t.string   "avatar"
+    t.text     "comment"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
